bef3733cd68b027c003653ca5f054e552426fca6
/usr/bin/vi <support@beget.ru>
2019-04-02 12:29:30 +0300

    first commit

---
+++ src/AlaskaServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace Aqoomen\Alaska;
+
+use Illuminate\Support\ServiceProvider;
+use Helori\LaravelCms\Models\Element;
+use Illuminate\Support\Facades\Route;
+
+class CmsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/laravel-cms.php', 'laravel-cms'
+        );
+    }
+
+    public function boot()
+	{
+        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+		$this->loadViewsFrom(__DIR__.'/views', 'laravel-cms');
+        $this->publishes([
+            __DIR__.'/config/laravel-cms.php' => config_path('laravel-cms.php'),
+        ], 'laravel-cms-config');
+
+        /*$this->publishes([
+            __DIR__.'/views' => base_path('resources/views/vendor/laravel-cms'),
+        ], 'laravel-cms-views');*/
+        $this->publishes([
+            __DIR__.'/components' => base_path('resources/assets/js/components/laravel-cms'),
+        ], 'laravel-cms-components');
+        $this->publishes([
+            __DIR__.'/assets/admin.js' => base_path('resources/assets/js/admin.js'),
+            __DIR__.'/assets/admin.scss' => base_path('resources/assets/sass/admin.scss'),
+            __DIR__.'/assets/tinymce.scss' => base_path('resources/assets/sass/tinymce.scss'),
+        ], 'laravel-cms-assets');
+	}
+}
