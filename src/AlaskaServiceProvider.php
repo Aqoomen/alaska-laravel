d6482538bab9388c401ba4a9b23e7df14d536f42
/usr/bin/vi <support@beget.ru>
2019-04-03 15:30:11 +0300

    Update version 0.1.4

--- src/AlaskaServiceProvider.php
+++ src/AlaskaServiceProvider.php
@@ -17,17 +17,19 @@ class AlaskaServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views/public', 'alaska-public');
         $this->loadViewsFrom(__DIR__.'/views/admin', 'alaska-admin');
 
+        /*
         $this->publishes([
             __DIR__.'/views/public' => base_path('resources/views/vendor/alaska-public'),
         ]);
-
+        */
+        
         $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
         $this->loadMigrationsFrom(__DIR__.'/migrations');
 
 
 
 
-
+        /*
         $this->publishes([
             __DIR__.'/components' => base_path('resources/assets/js/components/laravel-cms'),
         ], 'laravel-cms-components');
@@ -35,6 +37,6 @@ class AlaskaServiceProvider extends ServiceProvider
             __DIR__.'/assets/admin.js' => base_path('resources/assets/js/admin.js'),
             __DIR__.'/assets/admin.scss' => base_path('resources/assets/sass/admin.scss'),
             __DIR__.'/assets/tinymce.scss' => base_path('resources/assets/sass/tinymce.scss'),
-        ], 'laravel-cms-assets');
+        ], 'laravel-cms-assets');*/
 	}
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,32 +4,32 @@
     Route::group(['prefix' => 'admin'], function ($router) {
 
         Route::get('login', [
-            'uses' => '\iPremium\Alaska\Controllers\LoginController@showLoginForm',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\LoginController@showLoginForm',
             'as' => 'admin-login'
         ]);
 
         Route::post('login', [
-            'uses' => '\iPremium\Alaska\Controllers\LoginController@login',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\LoginController@login',
             'as' => 'admin-post-login'
         ]);
         Route::post('logout', [
-            'uses' => '\iPremium\Alaska\Controllers\LoginController@logout',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\LoginController@logout',
             'as' => 'admin-logout'
         ]);
         Route::get('password-forgot', [
-            'uses' => '\iPremium\Alaska\Controllers\ForgotPasswordController@showLinkRequestForm',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\ForgotPasswordController@showLinkRequestForm',
             'as' => 'admin-password-forgot'
         ]);
         Route::post('password-email', [
-            'uses' => '\iPremium\Alaska\Controllers\ForgotPasswordController@sendResetLinkEmail',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail',
             'as' => 'admin-password-email'
         ]);
         Route::get('password-reset/{token}', [
-            'uses' => '\iPremium\Alaska\Controllers\ResetPasswordController@showResetForm',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\ResetPasswordController@showResetForm',
             'as' => 'admin-password-reset'
         ]);
         Route::post('password-reset', [
-            'uses' => '\iPremium\Alaska\Controllers\ResetPasswordController@reset',
+            'uses' => '\iPremium\Alaska\Controllers\Auth\ResetPasswordController@reset',
             'as' => 'admin-post-password-reset'
         ]);
 
